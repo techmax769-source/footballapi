48cf2491d2cf5d49d84d10877b1e0221a87615df
techmax769-source <techmax769@gmail.com>
2026-02-14 02:40:24 +0300

    Delete api directory

--- api/health.js
+++
@@ -1,9 +0,0 @@
-import { successResponse } from '../lib/responseWrapper.js';
-
-export default async function handler(req, res) {
-  return res.status(200).json(successResponse({
-    status: "ok",
-    uptime: process.uptime(),
-    memory: process.memoryUsage()
-  }));
-}
